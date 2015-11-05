84c1aa3420591e0a4d460ff8fe2d2b1a6c3af069
dbose-fairfax <debasish.bose@fairfaxmedia.com.au>
2015-11-05 17:53:13 +1100

    Initial commit

--- Gemfile
+++ Gemfile
@@ -1,3 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+
+gem 'mandrill-template-manager', path: '../mandrill-template-manager'
+
+gem 'pry'
+gem 'handlebars'
+gem 'nokogiri'
+
+gem 'sinatra-flash'
--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'rubygems'
 require 'sinatra'
+require 'pry'
+
+require 'mandrill'
+require 'mandrill_template/client'
+require 'mandrill_template/template'
+
+require 'handlebars'
+require 'nokogiri'
+require 'cgi'
+
+require 'sinatra/flash'
+
+def get_or_post(path, opts={}, &block)
+  get(path, opts, &block)
+  post(path, opts, &block)
+end
 
 configure do
   enable :sessions
@@ -11,33 +27,167 @@ helpers do
   end
 end
 
-before '/secure/*' do
-  unless session[:identity]
-    session[:previous_url] = request.path
-    @error = 'Sorry, you need to be logged in to visit ' + request.path
-    halt erb(:login_form)
+get '/' do
+  @remote_templates = remote_templates
+  generate_local_templates @remote_templates
+  erb :home
+end
+
+get_or_post '/templates/sync' do
+  generate_local_templates_unsafe remote_templates
+  redirect '/'
+end
+
+get_or_post '/templates/:slug/sync' do
+  template = MandrillClient.client.templates.info(params[:slug])
+  generate_local_template_unsafe template
+  redirect '/'
+end
+
+post '/code/*' do
+  File.open("views/#{params['splat'].first}.erb", "w") do |f|
+    f.write params[:code]
+  end
+  "OK"
+end
+
+get '/templates/:slug/render' do
+  ## Read test data for handlebars variables interpolation
+  #merge_vars =  params ? JSON.parse(File.read("params.json")) : []
+  #
+  ## Fetch the code
+  #template = MandrillClient.client.templates.info(params[:slug])
+  #
+  #handlebars = Handlebars::Context.new
+  #h_template = handlebars.compile(template['code'])
+  #h_template.call(localize_merge_vars(merge_vars))
+
+  erb(:template_layout, :locals => { :slug => params["slug"], :render_for_upload => false } ,:layout => false)
+end
+
+post '/templates/:slug/draft' do
+
+  upload_template params[:slug]
+  flash[:notice] = "#{params[:slug]} has been drafted successfully !"
+  redirect "/"
+
+end
+
+post '/templates/draft' do
+  local_templates.each do |local_template|
+    slug = local_template.split(".").first
+    puts "Uploading #{slug}..."
+    upload_template slug
+  end
+  flash[:notice] = "All templates have been drafted successfully !"
+  redirect "/"
+end
+
+post '/templates/:slug/publish' do
+  puts MandrillClient.client.templates.publish(params[:slug]).to_yaml
+  flash[:notice] = "#{params[:slug]} has been published successfully !"
+  redirect "/"
+end
+
+post '/templates/publish' do
+  local_templates.each do |local_template|
+    slug = local_template.split(".").first
+    puts MandrillClient.client.templates.publish(slug).to_yaml
+  end
+
+  flash[:notice] = "All templates have been published successfully !"
+  redirect "/"
+end
+
+def generate_local_templates(remote_templates, path = "views/templates/")
+  if Dir["#{path}*"].empty?
+    generate_local_templates_unsafe(remote_templates, path)
   end
 end
 
-get '/' do
-  erb 'Can you handle a <a href="/secure/place">secret</a>?'
+def generate_local_templates_unsafe(remote_templates, path = "views/templates/")
+  remote_templates.each do |template|
+    generate_local_template_unsafe template, path
+  end
 end
 
-get '/login/form' do
-  erb :login_form
+def generate_local_template_unsafe(template, path = "views/templates/")
+  File.open "#{path}#{template['slug']}.erb", "w+"  do |file|
+    file.write(extract_content_from_template(template))
+  end
 end
 
-post '/login/attempt' do
-  session[:identity] = params['username']
-  where_user_came_from = session[:previous_url] || '/'
-  redirect to where_user_came_from
+def remote_templates
+  @remote_templates ||= MandrillClient.client.templates.list.select{|t| !t['labels'].include?('layout') }
 end
 
-get '/logout' do
-  session.delete(:identity)
-  erb "<div class='alert alert-message'>Logged out</div>"
+def local_templates
+  @local_templates ||= Dir['views/templates/*']
 end
 
-get '/secure/place' do
-  erb 'This is a secret place that only <%=session[:identity]%> has access to!'
+def localize_merge_vars(merge_vars)
+  h = {}
+  merge_vars.each {|kv| h[kv["name"]] = kv["content"] }
+  h
+end
+
+def extract_content_from_template(template, content_selector = 'div#body-content')
+  begin
+    template_code = template['code']
+    slug = template['slug']
+
+    return nil if template_code.nil?
+    parser = Nokogiri::HTML.fragment(template_code)
+
+    # Need to delete <style> , <meta> tags etc.
+    ['title', 'style', 'meta'].each do |tag|
+      tag_parser = parser.css(tag) rescue []
+      tag_parser.remove unless tag_parser.empty?
+    end
+
+    #binding.pry if slug == 'advertiser-sale-agent-signup-paid'
+
+    parsed = parser.css(content_selector) rescue []
+    return template_code if parsed.empty?
+
+    begin
+      parsed.try(:inner_html)
+    rescue => e
+      puts e.backtrace
+      parsed.to_a.first.inner_html
+    end
+  rescue => e
+    puts e.backtrace
+  end
+end
+
+def remote_template_exists?(name)
+  begin
+    MandrillClient.client.templates.info(name)
+    true
+  rescue Mandrill::UnknownTemplateError
+    false
+  end
+end
+
+def upload_template(slug)
+  if remote_template_exists?(slug)
+    method = :update
+  else
+    method = :add
+  end
+
+  templated_content = erb(:template_layout,
+                          :locals => { :slug => slug, :render_for_upload => true },
+                          :layout => false)
+  result = MandrillClient.client.templates.send(method,
+                                                slug,
+                                                nil,
+                                                nil,
+                                                nil,
+                                                templated_content,
+                                                nil,
+                                                false, # publish
+                                                [])
+  puts result.to_yaml
 end
